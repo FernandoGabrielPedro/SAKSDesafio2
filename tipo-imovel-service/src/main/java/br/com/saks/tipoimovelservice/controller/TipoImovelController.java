ef6ce3e513642d5ee90ffab400d241afdecb5ff9
7755287 <7755287@U702619.univali.br>
2022-05-23 16:32:08 -0300

    CM - Tipo Imóvel para serviço de Imobiliária

---
+++ tipo-imovel-service/src/main/java/br/com/saks/tipoimovelservice/controller/TipoImovelController.java
@@ -0,0 +1,60 @@
+package br.com.saks.tipoimovelservice.controller;
+
+import br.com.saks.tipoimovelservice.model.TipoImovel;
+import br.com.saks.tipoimovelservice.repository.TipoImovelRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/tipoimovel")
+public class TipoImovelController {
+    
+    @Autowired
+    private TipoImovelRepository tipoImovelRepository;
+    
+    @GetMapping
+    public List<TipoImovel> listarTodos() {
+        return tipoImovelRepository.findAll();
+    }
+    
+    @GetMapping(value="/{id}")
+    public Optional<TipoImovel> listarPeloId(@PathVariable Long id) {
+        return tipoImovelRepository.findById(id);
+    }
+    
+    @PostMapping
+    public TipoImovel adicionar(@RequestBody TipoImovel cliente) {
+        return tipoImovelRepository.save(cliente);
+    }
+    
+    @PutMapping(value="/{id}")
+    public ResponseEntity editar(@PathVariable Long id, @RequestBody TipoImovel cliente) {
+        return tipoImovelRepository.findById(id)
+                .map(record -> {
+                    record.setNome(cliente.getNome());
+                    TipoImovel tipoImovelUpdated = tipoImovelRepository.save(record);
+                    return ResponseEntity.ok().body(tipoImovelUpdated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity deletar(@PathVariable Long id) {
+        return tipoImovelRepository.findById(id)
+                .map(record -> {
+                    tipoImovelRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    
+}
